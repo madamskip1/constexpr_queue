1f2411bd285ecb8a63ff1e7b3aa7e76948342e7d
Maciej Adamski <madamskip1@gmail.com>
2025-06-15 18:55:19 +0200

    Add tests to verify if queue can be used with different types
    ATM few tests wont compile
    Going to fix class to handle it

---
+++ tests/types_tests.cpp
@@ -0,0 +1,95 @@
+#include <gtest/gtest.h>
+#include <type_traits>
+
+#include <constexpr_queue.hpp>
+
+TEST(ConstexprQueueTypesTest, PrimitiveType)
+{
+    constexpr constexpr_queue<int, 5> queue;
+    EXPECT_TRUE(queue.empty());
+    EXPECT_EQ(queue.size(), 0);
+    EXPECT_EQ(queue.capacity(), 5);
+}
+
+TEST(ConstexprQueueTypesTest, StandradLibraryType)
+{
+    // Will not compile atm
+    // constexpr constexpr_queue<std::string, 5> queue;
+    // EXPECT_TRUE(queue.empty());
+    // EXPECT_EQ(queue.size(), 0);
+    // EXPECT_EQ(queue.capacity(), 5);
+}
+
+TEST(ConstexprQueueTypesTest, UserDefinedType_DefaultConstructible_TrivialyDestructible)
+{
+    class UserDefinedType
+    {
+        int value;
+    };
+
+    EXPECT_TRUE(std::is_default_constructible_v<UserDefinedType>);
+    EXPECT_TRUE(std::is_trivially_destructible_v<UserDefinedType>);
+
+    constexpr constexpr_queue<UserDefinedType, 5> queue;
+    EXPECT_TRUE(queue.empty());
+    EXPECT_EQ(queue.size(), 0);
+    EXPECT_EQ(queue.capacity(), 5);
+}
+
+TEST(ConstexprQueueTypesTest, UserDefinedType_NonDefaultConstructible_TrivialyDestructible)
+{
+    class UserDefinedType
+    {
+    public:
+        UserDefinedType(int v) : value(v) {}
+        int value;
+    };
+
+    EXPECT_FALSE(std::is_default_constructible_v<UserDefinedType>);
+    EXPECT_TRUE(std::is_trivially_destructible_v<UserDefinedType>);
+
+    // Will not compile atm as constexpr_queue requires default constructibility
+    // constexpr constexpr_queue<UserDefinedType, 5> queue;
+    // EXPECT_TRUE(queue.empty());
+    // EXPECT_EQ(queue.size(), 0);
+    // EXPECT_EQ(queue.capacity(), 5);
+}
+
+TEST(ConstexprQueueTypesTest, UserDefinedType_DefaultConstructible_NonTrivialDestructible)
+{
+    class UserDefinedType
+    {
+    public:
+        ~UserDefinedType() {}; // Non-trivial destructor
+        int value;
+    };
+
+    EXPECT_TRUE(std::is_default_constructible_v<UserDefinedType>);
+    EXPECT_FALSE(std::is_trivially_destructible_v<UserDefinedType>);
+
+    // Will not compile atm as constexpr_queue requires trivial destructibility
+    // constexpr constexpr_queue<UserDefinedType, 5> queue;
+    // EXPECT_TRUE(queue.empty());
+    // EXPECT_EQ(queue.size(), 0);
+    // EXPECT_EQ(queue.capacity(), 5);
+}
+
+TEST(ConstexprQueueTypesTest, UserDefinedType_NonDefaultConstructible_NonTrivialDestructible)
+{
+    class UserDefinedType
+    {
+    public:
+        UserDefinedType(int v) : value(v) {}
+        ~UserDefinedType() {}; // Non-trivial destructor
+        int value;
+    };
+
+    EXPECT_FALSE(std::is_default_constructible_v<UserDefinedType>);
+    EXPECT_FALSE(std::is_trivially_destructible_v<UserDefinedType>);
+
+    // Will not compile atm as constexpr_queue requires default constructibility
+    // constexpr constexpr_queue<UserDefinedType, 5> queue;
+    // EXPECT_TRUE(queue.empty());
+    // EXPECT_EQ(queue.size(), 0);
+    // EXPECT_EQ(queue.capacity(), 5);
+}
