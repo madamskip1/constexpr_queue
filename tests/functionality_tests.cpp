810721402bd2debb97e0433394714f4e09941b57
Maciej Adamski <madamskip1@gmail.com>
2025-06-15 18:47:32 +0200

    Add functionallity tests

---
+++ tests/functionality_tests.cpp
@@ -0,0 +1,126 @@
+#include <gtest/gtest.h>
+
+#include <constexpr_queue.hpp>
+
+TEST(ConstexprQueueFunctionalityTest, Push)
+{
+    constexpr_queue<int, 5> queue;
+    queue.push(55);
+    EXPECT_FALSE(queue.empty());
+    EXPECT_EQ(queue.size(), 1);
+    EXPECT_EQ(queue.front(), 55);
+    EXPECT_EQ(queue.back(), 55);
+
+    queue.push(66);
+    EXPECT_FALSE(queue.empty());
+    EXPECT_EQ(queue.size(), 2);
+    EXPECT_EQ(queue.front(), 55);
+    EXPECT_EQ(queue.back(), 66);
+}
+
+TEST(ConstexprQueueFunctionalityTest, Pop)
+{
+    constexpr_queue<int, 5> queue;
+    queue.push(55);
+    queue.push(66);
+
+    int popped_value = queue.pop();
+    EXPECT_EQ(popped_value, 55);
+    EXPECT_EQ(queue.size(), 1);
+    EXPECT_EQ(queue.front(), 66);
+    EXPECT_EQ(queue.back(), 66);
+
+    popped_value = queue.pop();
+    EXPECT_EQ(popped_value, 66);
+    EXPECT_TRUE(queue.empty());
+}
+
+TEST(ConstexprQueueFunctionalityTest, Emplace)
+{
+    constexpr_queue<int, 5> queue;
+    queue.emplace(77);
+    EXPECT_FALSE(queue.empty());
+    EXPECT_EQ(queue.size(), 1);
+    EXPECT_EQ(queue.front(), 77);
+    EXPECT_EQ(queue.back(), 77);
+
+    queue.emplace(88);
+    EXPECT_FALSE(queue.empty());
+    EXPECT_EQ(queue.size(), 2);
+    EXPECT_EQ(queue.front(), 77);
+    EXPECT_EQ(queue.back(), 88);
+}
+
+TEST(ConstexprQueueFunctionalityTest, Swap)
+{
+    constexpr_queue<int, 5> queue1;
+    queue1.push(1);
+    queue1.push(2);
+
+    constexpr_queue<int, 5> queue2;
+    queue2.push(3);
+    queue2.push(4);
+
+    queue1.swap(queue2);
+
+    EXPECT_EQ(queue1.size(), 2);
+    EXPECT_EQ(queue1.front(), 3);
+    EXPECT_EQ(queue1.back(), 4);
+
+    EXPECT_EQ(queue2.size(), 2);
+    EXPECT_EQ(queue2.front(), 1);
+    EXPECT_EQ(queue2.back(), 2);
+}
+
+TEST(ConstexprQueueFunctionalityTest, OperatorEquality)
+{
+    constexpr_queue<int, 5> queue1;
+    queue1.push(1);
+    queue1.push(2);
+
+    constexpr_queue<int, 5> queue2;
+    queue2.push(1);
+    queue2.push(2);
+
+    EXPECT_TRUE(queue1 == queue2);
+    EXPECT_FALSE(queue1 != queue2);
+
+    queue2.push(3);
+    EXPECT_FALSE(queue1 == queue2);
+    EXPECT_TRUE(queue1 != queue2);
+}
+TEST(ConstexprQueueFunctionalityTest, OperatorLessThan)
+{
+    constexpr_queue<int, 5> queue1;
+    queue1.push(1);
+    queue1.push(2);
+
+    constexpr_queue<int, 5> queue2;
+    queue2.push(1);
+    queue2.push(3);
+
+    EXPECT_TRUE(queue1 < queue2);
+    EXPECT_TRUE(queue1 <= queue2);
+
+    queue2.pop();
+    EXPECT_TRUE(queue1 < queue2);
+    EXPECT_TRUE(queue1 <= queue2);
+}
+
+TEST(ConstexprQueueFunctionalityTest, OperatorGreaterThan)
+{
+    constexpr_queue<int, 5> queue1;
+    queue1.push(1);
+    queue1.push(2);
+
+    constexpr_queue<int, 5> queue2;
+    queue2.push(1);
+    queue2.push(3);
+
+    EXPECT_FALSE(queue1 > queue2);
+    EXPECT_FALSE(queue1 >= queue2);
+
+    queue1.pop();
+    EXPECT_TRUE(queue1 > queue2);
+    EXPECT_TRUE(queue1 >= queue2);
+}
